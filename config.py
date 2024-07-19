ab62284353d4b41a64544f369abcd5af7ce3e3a1
Hikari Kawai <121547505+hikarikawai@users.noreply.github.com>
2024-07-20 00:24:34 +0700

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/JustRexkedua",
+    "",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
